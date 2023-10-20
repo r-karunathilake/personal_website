c99078f51311165bb2694c20c78a9e1735c0597b
Ravindu Karunathilake <karunath@ualberta.ca>
2023-10-20 01:03:01 -0600

    Added footer and 'to the top button'. Additionally, adjusted the media queries for the project section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
               md: "768px",
               lg: "1024px",
               xl: "1280px",
-           }
+           },
+    animation: {
+      'gradient': 'gradient 2s ease 0s infinite',
+    },
+    keyframes:{
+      gradient: {
+        '0%': {'background-position': 'left center'},
+        '50%': {'background-position': 'right center'},
+        '100%': {'background-position': 'left center'},
+      }
+    }
 	},
 
 	fontFamily:{
